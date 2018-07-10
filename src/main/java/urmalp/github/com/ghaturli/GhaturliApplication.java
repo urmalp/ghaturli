5d8fa25a44d2515ae70b06e27f0f0ee81cc3222e
urmalp <prateek.urmaliya@gmail.com>
2018-07-11 00:15:06 +0530

    minor tweaks

--- src/main/java/urmalp/github/com/ghaturli/GhaturliApplication.java
+++ src/main/java/urmalp/github/com/ghaturli/GhaturliApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GhaturliApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GhaturliApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GhaturliApplication.class, args);
+        BinarySearchImpl search = new BinarySearchImpl();
+        int result = search.binarySearch(new int[]{12, 14, 6}, 3);
+        System.out.println(result);
+        SpringApplication.run(GhaturliApplication.class, args);
+    }
 }
