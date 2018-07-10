579c054cdb7ebd96cd1a1c226105954044bff50a
urmalp <prateek.urmaliya@gmail.com>
2018-07-10 21:54:53 +0530

    Initial commit

---
+++ src/main/java/urmalp/github/com/ghaturli/GhaturliApplication.java
@@ -0,0 +1,12 @@
+package urmalp.github.com.ghaturli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaturliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaturliApplication.class, args);
+	}
+}
---
+++ src/test/java/urmalp/github/com/ghaturli/GhaturliApplicationTests.java
@@ -0,0 +1,16 @@
+package urmalp.github.com.ghaturli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GhaturliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
